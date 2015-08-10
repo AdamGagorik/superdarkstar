725e39e6f7ad5ac04ff80ea2c3d9c4518a201adb
Adam Gagorik <adam.gagorik@gmail.com>
2015-08-10 10:45:07 -0400

    Add create server branch script

---
+++ scripts/create_server.sh
@@ -0,0 +1,11 @@
+git checkout master
+git branch -D server
+git checkout -b server
+git rebase gitignore
+git rebase settings
+git rebase cmake
+git rebase print
+git rebase state
+git rebase rate
+git rebase commands
+git rebase NPC
