fb0bd611c7bad9c91bac13da2196873aa6adefad
Adam Gagorik <adam.gagorik@gmail.com>
2015-09-21 16:10:08 -0400

    Enable more checkers for clang scan-build

--- scripts/maintain/scan.sh
+++ scripts/maintain/scan.sh
@@ -10,7 +10,8 @@ rm -rf ./scan-build
 mkdir ./scan-build
 
 cd ./scan-build
-scan-build --use-cc /usr/bin/clang --use-c++ /usr/bin/clang++ -v cmake ..
-scan-build -v -k -analyze-headers -stats -o . make -j 4
+
+../../scripts/maintain/scan-build.sh --use-cc /usr/bin/clang --use-c++ /usr/bin/clang++ -v cmake ..
+../../scripts/maintain/scan-build.sh -v -k -analyze-headers -stats -o . make -j 4
 
 cd ${TOPLEVEL}
